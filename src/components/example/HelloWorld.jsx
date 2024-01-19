a54ec17339e9e62e831a38521c9ae92984053077
hduchesne <hduchesne@jahia.com>
2024-01-19 14:29:57 +0100

    npx loaded

---
+++ src/components/example/HelloWorld.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useServerContext, getNodeProps } from '@jahia/js-server-engine'
+
+export const HelloComponent = () => {
+    const { currentNode } = useServerContext();
+    const props = getNodeProps(currentNode, ['textHello']);
+    return (
+        <div>
+            <h1>{props.textHello}</h1>
+        </div>
+    )
+}
+
+HelloComponent.jahiaComponent = {
+    id: 'helloComponent',
+    nodeType: 'luxeJahiaDemo:hello',
+    displayName: 'Hello Component',
+    componentType: 'view'
+}
