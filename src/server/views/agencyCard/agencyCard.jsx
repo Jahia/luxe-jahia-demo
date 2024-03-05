6e20d08e2552205850ab51e7f7dc2efe3ab0bd15
Janin-Michel-Mathias <jmichelmathias@jahia.com>
2024-03-05 11:20:31 +0100

    Add agency components

---
+++ src/server/views/agencyCard/agencyCard.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useServerContext, getNodeProps, jUrl, JAddContentButtons } from '@jahia/js-server-engine';
+
+export const agencyCard = () => {
+    const { currentNode } = useServerContext();
+    const content = getNodeProps(currentNode, ['image', 'name', 'address', 'phone']);
+    return (
+        <a className="lux-agencyCard d-flex" href={jUrl({path: currentNode.getPath()})}>
+            <img src={jUrl({path: content.image.getPath()})}
+                className="lux-agencyCard_image me-4"
+                height="200"
+                width="200"/>
+            <div className="d-flex flex-column justify-content-center flex-fill">
+                <h2 className="my-0">{content.name}</h2>
+                {content.address && <p className="m-0">{content.address}</p>}
+                {content.phone && <p className="m-0">{content.phone}</p>}
+            </div>
+            <JAddContentButtons />
+        </a>
+    );
+}
+
+agencyCard.jahiaComponent = {
+    nodeType: 'luxe:agency',
+    name: 'default',
+    componentType: 'view'
+};
+    
