f925ef1597019ba3ab5730620ecf5d3af6fd0b7f
hduchesne <hduchesne@jahia.com>
2024-04-10 10:20:30 +0200

    static label i18n for footer

--- src/server/components/HtmlFooter.jsx
+++ src/server/components/HtmlFooter.jsx
@@ -2,30 +2,33 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import {Col, Row, Section} from './grid';
+import {useTranslation} from 'react-i18next';
 
 export const HtmlFooter = ({className}) => {
+    const {t} = useTranslation();
+
     return (
         <Section component="footer" className={clsx('lux-site-footer', className)}>
             <Row>
                 <Col className="col-3">
-                    <h5>Resources</h5>
+                    <h5>{t('footer.resources')}</h5>
                     <ul className="list-unstyled">
                         <li>
-                            <a href="">Academy</a>
+                            <a href="">{t('footer.academy')}</a>
                         </li>
                         <li>
-                            <a href="">Tutorials</a>
+                            <a href="">{t('footer.tutorial')}</a>
                         </li>
                         <li>
-                            <a href="">Source code</a>
+                            <a href="">{t('footer.sourceCode')}</a>
                         </li>
                     </ul>
                 </Col>
                 <Col>
-                    <h5>Backoffice</h5>
+                    <h5>{t('footer.backOffice')}</h5>
                     <ul className="list-unstyled">
                         <li>
-                            <a href="#">Connection</a>
+                            <a href="#">{t('footer.connection')}</a>
                         </li>
                     </ul>
                 </Col>
@@ -54,8 +57,7 @@ export const HtmlFooter = ({className}) => {
                 </Col>
                 <Col className="text-end">
                     <span>
-                        Copyrights © 2002-2024 All Rights Reserved by Jahia
-                        Solutions Group SA
+                        {t('footer.copyright', {currentDate: new Date().getFullYear()})}
                     </span>
                 </Col>
             </Row>
