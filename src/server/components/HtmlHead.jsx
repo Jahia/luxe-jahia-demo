e087bcf554b4fe009aa6d98af31b7ba1f3aa4221
Geofrey Flores <gflores@jahia.com>
2024-09-27 18:06:27 -0400

    BACKLOG-23136: Add SEO meta tags for luxe templates

--- src/server/components/HtmlHead.jsx
+++ src/server/components/HtmlHead.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {AddResources, buildUrl, useServerContext} from '@jahia/js-server-core';
 import {getCssPath} from '../config';
+import {SeoMetaTags} from './SeoMetaTags';
 
 export const HtmlHead = ({children}) => {
     const {renderContext, currentResource} = useServerContext();
@@ -12,6 +13,8 @@ export const HtmlHead = ({children}) => {
         <head>
             <meta charSet="utf-8"/>
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <SeoMetaTags/>
+
             <link rel="icon" type="image/png" href={buildUrl({value: `${modulePath}/assets/favicon-32x32.png`}, renderContext, currentResource)}/>
             <AddResources type="css" resources={buildUrl({value: `${cssPath}/styles.css`}, renderContext, currentResource)}/>
             {/* Styles specific for Edit Mode (Page Composer) */}
---
+++ src/server/components/SeoMetaTags.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {buildUrl, getNodeProps, useServerContext} from '@jahia/js-server-core';
+
+export const SeoMetaTags = () => {
+    const {currentNode, currentResource, renderContext} = useServerContext();
+
+    const isDisplayableNodeType = currentNode.isNodeType('jnt:page') ||
+        currentNode.isNodeType('jmix:mainResource');
+    if (!isDisplayableNodeType) {
+        return;
+    }
+
+    const {
+        'jcr:title': seoTitle,
+        'jcr:description': seoDescription,
+        openGraphImage,
+        seoKeywords
+    } = getNodeProps(currentNode, [
+        'jcr:title',
+        'jcr:description',
+        'openGraphImage',
+        'seoKeywords'
+    ]) || {};
+    const locale = currentResource.getLocale().getLanguage();
+    const absOgImageUrl = openGraphImage?.getAbsoluteUrl(renderContext.getRequest());
+
+    const getAbsoluteUrl = node => {
+        const server = renderContext.getURLGenerator().getServer();
+        const relUrl = buildUrl({path: node.getPath()}, renderContext, currentResource);
+        return `${server}${relUrl}`;
+    };
+
+    return (
+        <>
+            {absOgImageUrl && <meta property="og:image" content={absOgImageUrl}/>}
+            {seoTitle && <meta property="og:title" content={seoTitle}/>}
+            <meta property="og:url" content={getAbsoluteUrl(currentNode)}/>
+            {seoDescription &&
+                <>
+                    <meta property="og:description" content={seoDescription.toLocaleString(locale) || seoDescription}/>
+                    <meta name="description" content={seoDescription.toLocaleString(locale) || seoDescription}/>
+                </>}
+            {seoKeywords?.length && <meta name="keywords" content={seoKeywords.map(kw => kw.toLocaleString(locale)).join(',')}/>}
+        </>
+    );
+};
