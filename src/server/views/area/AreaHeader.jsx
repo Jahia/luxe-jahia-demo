4b81e32ec32ec49af3d2ef03aabfd6e251bd946a
hduchesne <hduchesne@jahia.com>
2025-02-06 17:46:25 +0100

    remove unused section from header form

--- src/server/views/area/AreaHeader.jsx
+++ src/server/views/area/AreaHeader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {
     defineJahiaComponent, Render, AddContentButtons
 } from '@jahia/javascript-modules-library';
-
+// TODO manage subnodeview
 export const AreaHeader = () => (
     <>
         <Render path="content"
