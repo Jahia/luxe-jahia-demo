a5fb7368c0d0573edcd189da02fd01a59b088ce9
Gautier Ben Aïm <benaim.gautier@gmail.com>
2025-06-17 10:52:29 +0200

    tankafaire

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,3 +1,4 @@
+// @ts-check
 import jahia from "@jahia/vite-plugin";
 import { spawnSync } from "node:child_process";
 import path from "node:path";
@@ -12,20 +13,6 @@ export default defineConfig({
   plugins: [
     sbom({ specVersion: "1.4" }),
     jahia({
-      client: {
-        input: {
-          dir: "./src/",
-          glob: "**/*.client.{jsx,tsx}",
-        },
-        output: "./dist/",
-      },
-      server: {
-        input: "./src/**/*.server.{jsx,tsx}",
-        output: {
-          dir: "./dist/server/",
-          fileName: "index", // Will produce index.js and style.css (because of a bug)
-        },
-      },
       // This function is called every time a build succeeds in watch mode
       watchCallback() {
         spawnSync("yarn", ["watch:callback"], { stdio: "inherit" });
