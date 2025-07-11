bb62edf63745498f674f630b7777506f553a0b78
Félix Vallé <fvalle@jahia.com>
2025-07-11 18:25:24 +0200

    feat: gallery with modal

--- src/commons/index.ts
+++ src/commons/index.ts
@@ -1,5 +1,6 @@
 export * from "./PageTitle";
 export * from "./Figure";
+// export * from "./Gallery.client";
 export * from "./ContentHeader";
 export * from "./List";
 export * from "./HeadingSection";
