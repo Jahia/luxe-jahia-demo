808bfbed4990f7e6201548bfdf273c6d72800328
Gautier Ben Aïm <48261497+GauBen@users.noreply.github.com>
2025-07-30 11:37:23 +0200

    fix: allow raw html in Row (#287)

--- src/commons/grid/Row.tsx
+++ src/commons/grid/Row.tsx
@@ -6,6 +6,7 @@ export const Row = <T,>({
 	className,
 	component: Component = "div",
 	children,
+	...props
 }: HTMLAttributes<T> & { component?: ElementType }) => (
-	<Component className={clsx(classes.row, className)}>{children}</Component>
+	<Component className={clsx(classes.row, className)} {...props}>{children}</Component>
 );
