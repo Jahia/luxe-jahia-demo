4ec74da11b8bb5e2d4f21a583d35be0b00f77544
Gautier Ben Aïm <benaim.gautier@gmail.com>
2025-07-31 14:55:25 +0200

    feat: self-host fonts

--- packages/design-system/src/index.ts
+++ packages/design-system/src/index.ts
@@ -1,3 +1,6 @@
+import "@fontsource-variable/inter";
+import "@fontsource-variable/playfair-display";
 import "./variables.css";
+import "./global.css";
 
 export { Badge } from "./Badge/index.tsx";
