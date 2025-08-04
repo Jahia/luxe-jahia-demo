bcd8c5c6087df43539ffdfd4dd64eb303d1cc916
hduchesne <hduchesne@jahia.com>
2025-08-04 18:29:43 +0200

    start the story but there is issue

---
+++ packages/design-system/src/Dialog/stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { Dialog } from "./index.tsx";
+
+const children = <p style={{ background: "#fff" }}>The dialog content</p>;
+// More on writing stories: https://storybook.js.org/docs/writing-stories
+const meta = {
+	title: "Atoms/Dialog",
+	component: Dialog,
+	tags: ["autodocs"],
+	args: { title: "The title", children, isOpen: true },
+} satisfies Meta<typeof Dialog>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
