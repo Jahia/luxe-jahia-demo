c9c5f4638acc3d3b0d8f16756d4cdb01b07a59fd
hduchesne <hduchesne@jahia.com>
2025-08-06 10:52:38 +0200

    update responsive

--- packages/design-system/src/Dialog/stories.tsx
+++ packages/design-system/src/Dialog/stories.tsx
@@ -1,16 +1,18 @@
-import type { Meta, StoryObj } from "@storybook/react-vite";
-import { Dialog } from "./index.tsx";
+//TODO fix the css import issue with Storybook
 
-const children = <p style={{ background: "#fff" }}>The dialog content</p>;
-// More on writing stories: https://storybook.js.org/docs/writing-stories
-const meta = {
-	title: "Atoms/Dialog",
-	component: Dialog,
-	tags: ["autodocs"],
-	args: { title: "The title", children, isOpen: true },
-} satisfies Meta<typeof Dialog>;
-
-export default meta;
-type Story = StoryObj<typeof meta>;
-
-export const Default: Story = {};
+// import type { Meta, StoryObj } from "@storybook/react-vite";
+// import { Dialog } from "./index.tsx";
+//
+// const children = <p style={{ background: "#fff" }}>The dialog content</p>;
+// // More on writing stories: https://storybook.js.org/docs/writing-stories
+// const meta = {
+// 	title: "Atoms/Dialog",
+// 	component: Dialog,
+// 	tags: ["autodocs"],
+// 	args: { title: "The title", children, isOpen: true },
+// } satisfies Meta<typeof Dialog>;
+//
+// export default meta;
+// type Story = StoryObj<typeof meta>;
+//
+// export const Default: Story = {};
