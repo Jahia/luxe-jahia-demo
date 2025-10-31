342d73c386a6433aab4ebe172f1a8ee48990ed35
Gautier Ben Aïm <benaim.gautier@gmail.com>
2025-10-31 13:07:04 +0100

    factor constraints

--- packages/template-set/src/commons/libs/jcrQueryBuilder/index.ts
+++ packages/template-set/src/commons/libs/jcrQueryBuilder/index.ts
@@ -1,5 +1,6 @@
 import { graphql, type TadaDocumentNode } from "gql.tada";
 import { print } from "@0no-co/graphql.web";
+import type { JCRQueryConfig } from "./types.ts";
 
 export async function execute<Result = any, Variables = any>(
 	query: TadaDocumentNode<Result, Variables>,
@@ -48,3 +49,21 @@ export const gqlNodesQuery = graphql(`
 		}
 	}
 `);
+
+export const getCriteria = (params: Record<string, string[]>, config: JCRQueryConfig) => {
+	const constraints = Object.entries(params)
+		.map(([param, values]) => ({
+			any: values.map((value) => ({ property: param, equals: value })),
+		}))
+		// Remove constraints with no values
+		.filter(({ any }) => any.length > 0);
+
+	return graphql.scalar("InputGqlJcrNodeCriteriaInput", {
+		nodeType: config.type,
+		nodeConstraint: constraints.length > 0 ? { all: constraints } : null,
+		ordering: {
+			property: config.criteria,
+			orderType: config.sortDirection.toUpperCase() as "ASC" | "DESC",
+		},
+	});
+};
