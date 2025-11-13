cc35bf70f8431515b5a965a38592da77fed61b11
hduchesne <hduchesne@jahia.com>
2025-11-13 15:36:43 +0100

    udpate the image url builder to use the new "args" props instead of parameters

--- packages/template-set/src/commons/libs/imageNodeToProps/meta.ts
+++ packages/template-set/src/commons/libs/imageNodeToProps/meta.ts
@@ -53,7 +53,7 @@ export const sizedUrlOrOriginal = (
 		noParams || allNoop
 			? undefined
 			: {
-					parameters: {
+					args: {
 						...(requestedW != null && { w: String(requestedW) }),
 						...(requestedH != null && { h: String(requestedH) }),
 					},
